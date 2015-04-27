1c0e9d2c6d65171539f846e965873bed3b2a4402
Torsten Robitzki <Torsten@Robitzki.de>
2015-04-27 15:36:34 +0200

    let client_characteristic_configuration be a pointer to a dense array of configuration bits

--- bluetoe/client_characteristic_configuration.hpp
+++ bluetoe/client_characteristic_configuration.hpp
@@ -9,43 +9,75 @@ namespace details {
     class client_characteristic_configuration
     {
     public:
-        client_characteristic_configuration() : data_( 0 )
+        client_characteristic_configuration()
+            : data_( nullptr )
+            , size_( 0 )
         {
         }
 
-        std::uint16_t flags() const
+        // if there are no bugs, data will be null when there is no attribute access function, that will call flags()
+        explicit client_characteristic_configuration( std::uint8_t* data, std::size_t s )
+            : data_( data )
+            , size_( s )
         {
-            return data_;
         }
 
-        void flags( std::uint16_t new_flags )
+        std::uint16_t flags( std::size_t index ) const
         {
-            data_ = new_flags;
+            assert( data_ );
+            assert( index < size_ );
+
+            return ( data_[ index / 4 ] >> shift( index ) ) & 0x3;
         }
+
+        void flags( std::size_t index, std::uint16_t new_flags )
+        {
+            assert( data_ );
+            assert( index < size_ );
+
+            // do not assert on new_flags as they might come from a client
+            data_[ index / 4 ] = ( data_[ index / 4 ] & ~mask( index ) ) | ( ( new_flags & 0x03 ) << shift( index ) );
+        }
+
+        static constexpr std::size_t bits_per_config = 2;
+
     private:
-        std::uint16_t   data_;
+        static std::size_t shift( std::size_t index )
+        {
+            return ( index % 4 ) * bits_per_config;
+        }
+
+        static std::uint8_t mask( std::size_t index )
+        {
+            return 0x03 << shift( index );
+        }
+
+        std::uint8_t*   data_;
+
+        // this member is purly for debugging and can be removed
+        std::size_t     size_;
     };
 
     template < std::size_t Size >
     class client_characteristic_configurations
     {
     public:
-        client_characteristic_configuration* client_configurations()
+        client_characteristic_configuration client_configurations()
         {
-            return &configs_[ 0 ];
+            return client_characteristic_configuration( &configs_[ 0 ], Size );
         };
 
     private:
-        client_characteristic_configuration configs_[ Size ];
+        std::uint8_t configs_[ ( Size * client_characteristic_configuration::bits_per_config + 7 ) / 8 ];
     };
 
     template <>
     class client_characteristic_configurations< 0 >
     {
     public:
-        client_characteristic_configuration* client_configurations()
+        client_characteristic_configuration client_configurations()
         {
-            return nullptr;
+            return client_characteristic_configuration();
         }
     };
 
--- tests/server_tests.cpp
+++ tests/server_tests.cpp
@@ -7,24 +7,36 @@ BOOST_AUTO_TEST_SUITE( characteristic_configuration )
 
 std::uint16_t temperature_value1 = 0x0104;
 std::uint16_t temperature_value2 = 0x0104;
+std::uint16_t temperature_value3 = 0x0104;
+std::uint16_t temperature_value4 = 0x0104;
+std::uint16_t temperature_value5 = 0x0104;
+std::uint16_t temperature_value6 = 0x0104;
+std::uint16_t temperature_value7 = 0x0104;
+std::uint16_t temperature_value8 = 0x0104;
+std::uint16_t temperature_value9 = 0x0104;
+
+template < std::uint16_t* P, std::uint16_t ID >
+using temp_char = bluetoe::characteristic<
+    bluetoe::characteristic_uuid16< ID >,
+    bluetoe::bind_characteristic_value< std::uint16_t, P >,
+    bluetoe::notify >;
 
 typedef bluetoe::server<
     bluetoe::service<
         bluetoe::service_uuid< 0x8C8B4094, 0x0DE2, 0x499F, 0xA28A, 0x4EED5BC73CA9 >,
-        bluetoe::characteristic<
-            bluetoe::characteristic_uuid< 0x8C8B4094, 0x0DE2, 0x499F, 0xA28A, 0x4EED5BC73CAA >,
-            bluetoe::bind_characteristic_value< decltype( temperature_value1 ), &temperature_value1 >,
-            bluetoe::notify
-        >,
-        bluetoe::characteristic<
-            bluetoe::characteristic_uuid< 0x8C8B4094, 0x0DE2, 0x499F, 0xA28A, 0x4EED5BC73CAA >,
-            bluetoe::bind_characteristic_value< decltype( temperature_value2 ), &temperature_value2 >,
-            bluetoe::notify
-        >
+        temp_char< &temperature_value1, 1 >,
+        temp_char< &temperature_value2, 2 >,
+        temp_char< &temperature_value3, 3 >,
+        temp_char< &temperature_value4, 4 >,
+        temp_char< &temperature_value5, 5 >,
+        temp_char< &temperature_value6, 6 >,
+        temp_char< &temperature_value7, 7 >,
+        temp_char< &temperature_value8, 8 >,
+        temp_char< &temperature_value9, 9 >
     >
-> small_temperature_service;
+> large_temperature_service;
 
-template < class Server = small_temperature_service >
+template < class Server = large_temperature_service >
 struct connection_data : Server::connection_data
 {
     connection_data() : Server::connection_data( 23 ) {}
@@ -32,41 +44,39 @@ struct connection_data : Server::connection_data
 
 BOOST_FIXTURE_TEST_CASE( characteristic_configuration_is_initialized_with_zereo, connection_data<> )
 {
-    BOOST_CHECK_EQUAL( 0, client_configurations()[ 0 ].flags() );
-    BOOST_CHECK_EQUAL( 0, client_configurations()[ 1 ].flags() );
+    BOOST_CHECK_EQUAL( 0, client_configurations().flags( 0 ) );
+    BOOST_CHECK_EQUAL( 0, client_configurations().flags( 1 ) );
+    // ...
+    BOOST_CHECK_EQUAL( 0, client_configurations().flags( 8 ) );
 }
 
 BOOST_FIXTURE_TEST_CASE( is_writeable, connection_data<> )
 {
-    client_configurations()[ 0 ].flags( 0x3 );
-    BOOST_CHECK_EQUAL( 0x3, client_configurations()[ 0 ].flags() );
+    client_configurations().flags( 0, 0x3 );
+    BOOST_CHECK_EQUAL( 0x3, client_configurations().flags( 0 ) );
 }
 
 BOOST_FIXTURE_TEST_CASE( configurations_are_independent, connection_data<> )
 {
-    client_configurations()[ 0 ].flags( 0x3 );
-    client_configurations()[ 1 ].flags( 0x2 );
-    BOOST_CHECK_EQUAL( 0x3, client_configurations()[ 0 ].flags() );
-    BOOST_CHECK_EQUAL( 0x2, client_configurations()[ 1 ].flags() );
+    client_configurations().flags( 0, 0x3 );
+    client_configurations().flags( 1, 0x2 );
+    client_configurations().flags( 2, 0x1 );
+    client_configurations().flags( 3, 0x1 );
+    client_configurations().flags( 4, 0x2 );
+    client_configurations().flags( 5, 0x3 );
+    BOOST_CHECK_EQUAL( 0x3, client_configurations().flags( 0 ) );
+    BOOST_CHECK_EQUAL( 0x2, client_configurations().flags( 1 ) );
+    BOOST_CHECK_EQUAL( 0x1, client_configurations().flags( 2 ) );
+    BOOST_CHECK_EQUAL( 0x1, client_configurations().flags( 3 ) );
+    BOOST_CHECK_EQUAL( 0x2, client_configurations().flags( 4 ) );
+    BOOST_CHECK_EQUAL( 0x3, client_configurations().flags( 5 ) );
 }
 
-typedef bluetoe::server<
-    bluetoe::service<
-        bluetoe::service_uuid< 0x8C8B4094, 0x0DE2, 0x499F, 0xA28A, 0x4EED5BC73CA9 >,
-        bluetoe::characteristic<
-            bluetoe::characteristic_uuid< 0x8C8B4094, 0x0DE2, 0x499F, 0xA28A, 0x4EED5BC73CAA >,
-            bluetoe::bind_characteristic_value< decltype( temperature_value1 ), &temperature_value1 >
-        >,
-        bluetoe::characteristic<
-            bluetoe::characteristic_uuid< 0x8C8B4094, 0x0DE2, 0x499F, 0xA28A, 0x4EED5BC73CAA >,
-            bluetoe::bind_characteristic_value< decltype( temperature_value2 ), &temperature_value2 >
-        >
-    >
-> no_notification_service;
-
-BOOST_FIXTURE_TEST_CASE( if_no_client_configuration_is_required_no_data_is_used, connection_data< no_notification_service > )
+BOOST_FIXTURE_TEST_CASE( is_clearable, connection_data<> )
 {
-    BOOST_CHECK( client_configurations() == nullptr );
+    client_configurations().flags( 7, 0x3 );
+    client_configurations().flags( 7, 0x1 );
+    BOOST_CHECK_EQUAL( 0x1, client_configurations().flags( 7 ) );
 }
 
 BOOST_AUTO_TEST_SUITE_END()
