240693282e1f57ca8383cb0af48edcb792597f98
Torsten Robitzki <Torsten@Robitzki.de>
2015-09-29 13:20:59 +0200

    workaround for bug in clang

--- bluetoe/characteristic_value.hpp
+++ bluetoe/characteristic_value.hpp
@@ -81,94 +81,72 @@ namespace bluetoe {
             static constexpr bool has_write_access = !std::is_const< T >::value && !details::has_option< no_write_access, Options... >::value;
             static constexpr bool has_notifcation  = details::has_option< notify, Options... >::value;
 
-            static details::attribute_access_result characteristic_value_access( details::attribute_access_arguments&, std::uint16_t attribute_handle );
-            static bool is_this( const void* value );
+            static details::attribute_access_result characteristic_value_access( details::attribute_access_arguments& args, std::uint16_t attribute_handle )
+            {
+                if ( args.type == details::attribute_access_type::read )
+                {
+                    return characteristic_value_read_access( args, std::integral_constant< bool, has_read_access >() );
+                }
+                else if ( args.type == details::attribute_access_type::write )
+                {
+                    return characteristic_value_write_access( args, std::integral_constant< bool, has_write_access >() );
+                }
+
+                return details::attribute_access_result::write_not_permitted;
+            }
+
+            static bool is_this( const void* value )
+            {
+                return value == Ptr;
+            }
+
         private:
-            static details::attribute_access_result characteristic_value_read_access( details::attribute_access_arguments&, const std::true_type& );
-            static details::attribute_access_result characteristic_value_read_access( details::attribute_access_arguments&, const std::false_type& );
-            static details::attribute_access_result characteristic_value_write_access( details::attribute_access_arguments&, const std::true_type& );
-            static details::attribute_access_result characteristic_value_write_access( details::attribute_access_arguments&, const std::false_type& );
+            static details::attribute_access_result characteristic_value_read_access( details::attribute_access_arguments& args, const std::true_type& )
+            {
+                if ( args.buffer_offset > sizeof( T ) )
+                    return details::attribute_access_result::invalid_offset;
+
+                args.buffer_size = std::min< std::size_t >( args.buffer_size, sizeof( T ) - args.buffer_offset );
+                const std::uint8_t* const ptr = static_cast< const std::uint8_t* >( static_cast< const void* >( Ptr ) ) + args.buffer_offset;
+
+                std::copy( ptr, ptr + args.buffer_size, args.buffer );
+
+                return args.buffer_size == sizeof( T ) - args.buffer_offset
+                    ? details::attribute_access_result::success
+                    : details::attribute_access_result::read_truncated;
+            }
+
+            static details::attribute_access_result characteristic_value_read_access( details::attribute_access_arguments&, const std::false_type& )
+            {
+                return details::attribute_access_result::read_not_permitted;
+            }
+
+            static details::attribute_access_result characteristic_value_write_access( details::attribute_access_arguments& args, const std::true_type& )
+            {
+                if ( args.buffer_offset > sizeof( T ) )
+                    return details::attribute_access_result::invalid_offset;
+
+                if ( args.buffer_size + args.buffer_offset > sizeof( T ) )
+                    return details::attribute_access_result::write_overflow;
+
+                args.buffer_size = std::min< std::size_t >( args.buffer_size, sizeof( T ) - args.buffer_offset );
+
+                std::uint8_t* const ptr = static_cast< std::uint8_t* >( static_cast< void* >( Ptr ) );
+                std::copy( args.buffer, args.buffer + args.buffer_size, ptr + args.buffer_offset );
+
+                return details::attribute_access_result::success;
+            }
+
+            static details::attribute_access_result characteristic_value_write_access( details::attribute_access_arguments&, const std::false_type& )
+            {
+                return details::attribute_access_result::write_not_permitted;
+            }
+
         };
 
         struct meta_type : details::characteristic_value_meta_type, details::characteristic_value_declaration_parameter {};
         /** @endcond */
     };
-
-    // implementation
-    /** @cond HIDDEN_SYMBOLS */
-    template < typename T, T* Ptr >
-    template < typename ... Options >
-    details::attribute_access_result bind_characteristic_value< T, Ptr >::value_impl< Options... >::characteristic_value_access( details::attribute_access_arguments& args, std::uint16_t attribute_handle )
-    {
-        if ( args.type == details::attribute_access_type::read )
-        {
-            return characteristic_value_read_access( args, std::integral_constant< bool, has_read_access >() );
-        }
-        else if ( args.type == details::attribute_access_type::write )
-        {
-            return characteristic_value_write_access( args, std::integral_constant< bool, has_write_access >() );
-        }
-
-        return details::attribute_access_result::write_not_permitted;
-    }
-
-    template < typename T, T* Ptr >
-    template < typename ... Options >
-    details::attribute_access_result bind_characteristic_value< T, Ptr >::value_impl< Options... >::characteristic_value_read_access( details::attribute_access_arguments& args, const std::true_type& )
-    {
-        if ( args.buffer_offset > sizeof( T ) )
-            return details::attribute_access_result::invalid_offset;
-
-        args.buffer_size = std::min< std::size_t >( args.buffer_size, sizeof( T ) - args.buffer_offset );
-        const std::uint8_t* const ptr = static_cast< const std::uint8_t* >( static_cast< const void* >( Ptr ) ) + args.buffer_offset;
-
-        std::copy( ptr, ptr + args.buffer_size, args.buffer );
-
-        return args.buffer_size == sizeof( T ) - args.buffer_offset
-            ? details::attribute_access_result::success
-            : details::attribute_access_result::read_truncated;
-    }
-
-    template < typename T, T* Ptr >
-    template < typename ... Options >
-    details::attribute_access_result bind_characteristic_value< T, Ptr >::value_impl< Options... >::characteristic_value_read_access( details::attribute_access_arguments&, const std::false_type& )
-    {
-        return details::attribute_access_result::read_not_permitted;
-    }
-
-    template < typename T, T* Ptr >
-    template < typename ... Options >
-    details::attribute_access_result bind_characteristic_value< T, Ptr >::value_impl< Options... >::characteristic_value_write_access( details::attribute_access_arguments& args, const std::true_type& )
-    {
-        if ( args.buffer_offset > sizeof( T ) )
-            return details::attribute_access_result::invalid_offset;
-
-        if ( args.buffer_size + args.buffer_offset > sizeof( T ) )
-            return details::attribute_access_result::write_overflow;
-
-        args.buffer_size = std::min< std::size_t >( args.buffer_size, sizeof( T ) - args.buffer_offset );
-
-        std::uint8_t* const ptr = static_cast< std::uint8_t* >( static_cast< void* >( Ptr ) );
-        std::copy( args.buffer, args.buffer + args.buffer_size, ptr + args.buffer_offset );
-
-        return details::attribute_access_result::success;
-    }
-
-    template < typename T, T* Ptr >
-    template < typename ... Options >
-    details::attribute_access_result bind_characteristic_value< T, Ptr >::value_impl< Options... >::characteristic_value_write_access( details::attribute_access_arguments&, const std::false_type& )
-    {
-        return details::attribute_access_result::write_not_permitted;
-    }
-
-    template < typename T, T* Ptr >
-    template < typename ... Options >
-    bool bind_characteristic_value< T, Ptr >::value_impl< Options... >::is_this( const void* value )
-    {
-        return value == Ptr;
-    }
-
-    /** @endcond */
 }
 
 #endif
