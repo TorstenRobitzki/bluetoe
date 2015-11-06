4874cd27bf505433d5564144f45d9a12b6d51353
Torsten Robitzki <Torsten@Robitzki.de>
2015-11-06 22:11:20 +0100

    appearace for GAP service

--- bluetoe/gap_service.hpp
+++ bluetoe/gap_service.hpp
@@ -3,6 +3,7 @@
 
 #include <cstdint>
 #include <bluetoe/service.hpp>
+#include <bluetoe/appearance.hpp>
 
 namespace bluetoe {
 
@@ -33,30 +34,35 @@ namespace bluetoe {
         };
     };
 
-    struct gap_service_for_gatt_servers {
-        static const std::uint16_t dummy;
+    struct gap_service_for_gatt_servers
+    {
         typedef details::gap_service_definition_meta_type meta_type;
 
-        typedef service<
-            // TODO, use the correct uuid
-            service_uuid16< 0x4711 >,
-            characteristic<
-                characteristic_uuid16< 0x2A00 >,
-                fixed_uint16_value< 0 > // TODO resonable default
-            >,
-            characteristic<
-                characteristic_uuid16< 0x2A01 >,
-                fixed_uint16_value< 0 > // TODO resonable default
-            >
-        > gap_service;
+        template < typename Appearance >
+        using gap_service =
+            service<
+                service_uuid16< 0x1800 >,
+                characteristic<
+                    characteristic_uuid16< 0x2A00 >,
+                    fixed_uint16_value< 0 > // TODO resonable default
+                >,
+                characteristic<
+                    characteristic_uuid16< 0x2A01 >,
+                    fixed_uint16_value< Appearance::value >
+                >
+            >;
 
         template < typename Services, typename ... ServerOptions >
         struct add_service {
-            typedef typename details::add_type< Services, gap_service >::type type;
+            typedef typename details::find_by_meta_type<
+                details::device_appearance_meta_type,
+                ServerOptions...,
+                appearance::unknown
+            >::type device_appearance;
+
+            typedef typename details::add_type< Services, gap_service< device_appearance > >::type type;
         };
     };
-
-    const std::uint16_t gap_service_for_gatt_servers::dummy = 0;
 }
 
 #endif
--- tests/gap_service_tests.cpp
+++ tests/gap_service_tests.cpp
@@ -24,7 +24,7 @@ typedef bluetoe::server<
 BOOST_FIXTURE_TEST_CASE( service_is_discoverable_by_default, request_with_reponse< nupsy_service > )
 {
     // Find By Type Value Request, 1, 0xffff, <<primary service>>, <<gap service>>
-    l2cap_input( { 0x06, 0x01, 0x00, 0xff, 0xff, 0x00, 0x28, 0x11, 0x47 } );
+    l2cap_input( { 0x06, 0x01, 0x00, 0xff, 0xff, 0x00, 0x28, 0x00, 0x18 } );
     expected_result( {
         0x07,
         0x04, 0x00, 0x08, 0x00
@@ -69,6 +69,28 @@ BOOST_FIXTURE_TEST_CASE( no_service_no_appearance, request_with_reponse< without
         { 0x08, 0x01, 0x00, 0xff, 0xff, 0x01, 0x2A },
         0x08, 0x0001, 0x0a
     );
+}
+
+typedef bluetoe::server<
+    bluetoe::service<
+        bluetoe::service_uuid< 0x8C8B4094, 0x0DE2, 0x499F, 0xA28A, 0x4EED5BC73CA9 >,
+        bluetoe::characteristic<
+            bluetoe::characteristic_uuid< 0x8C8B4094, 0x0DE2, 0x499F, 0xA28A, 0x4EED5BC73CAA >,
+            bluetoe::bind_characteristic_value< decltype( nupsy ), &nupsy >,
+            bluetoe::no_write_access
+        >
+    >,
+    bluetoe::appearance::location_and_navigation_display_device
+> location_and_navigation_display_device_server;
+
+BOOST_FIXTURE_TEST_CASE( configured_appearance_is_used, request_with_reponse< location_and_navigation_display_device_server > )
+{
+    // Read by Type Request, 0x0001, 0xffff, 0x2A01
+    l2cap_input( { 0x08, 0x01, 0x00, 0xff, 0xff, 0x01, 0x2A } );
+    expected_result( {
+        0x09,
+        0x04, 0x08, 0x00, 0x42, 0x14
+    } );
 }
 
 // BOOST_FIXTURE_TEST_CASE( device_name_is_mandatory, request_with_reponse< nupsy_service > )
