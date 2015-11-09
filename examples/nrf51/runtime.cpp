d5cfac4ab86c42f6b31be95367fc5a515d0d0390
Torsten Robitzki <Torsten@Robitzki.de>
2015-11-09 15:49:12 +0100

    zero initialize bss data

--- examples/nrf51/runtime.cpp
+++ examples/nrf51/runtime.cpp
@@ -1,3 +1,5 @@
+#include <cstdint>
+
 extern "C" void __cxa_pure_virtual(void) {}
 
 extern "C" void (*__preinit_array_start []) (void) __attribute__((weak));
@@ -9,9 +11,15 @@ extern "C" void (*__fini_array_end []) (void) __attribute__((weak));
 
 extern "C" typedef void (*vector)();
 
+extern "C" std::uint32_t __bss_start__;
+extern "C" std::uint32_t __bss_end__;
+
 extern "C" void _start(void) {
     extern int main(void);
 
+    // zero initialize static allocated variables
+    for ( std::uint32_t* bss = &__bss_start__; bss != &__bss_end__; ++bss )
+        *bss = 0;
 
     for ( vector* init = &__preinit_array_start[ 0 ]; init != &__preinit_array_end[ 0 ]; ++init )
         (*init)();
