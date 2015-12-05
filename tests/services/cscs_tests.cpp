f0e701bf272b9c7b1ae9bc450877512e6afceb16
Torsten Robitzki <Torsten@Robitzki.de>
2015-12-05 19:23:39 +0100

    check client characteristic configurations

--- tests/services/cscs_tests.cpp
+++ tests/services/cscs_tests.cpp
@@ -22,12 +22,12 @@ struct discover_primary_service : request_with_reponse< csc_server, 100u >
 
         BOOST_REQUIRE_EQUAL( response_size, 5u );
 
-        starting_handle = response[ 1 ] | ( response[ 2 ] << 8 );
-        ending_handle   = response[ 3 ] | ( response[ 4 ] << 8 );
+        service_starting_handle = response[ 1 ] | ( response[ 2 ] << 8 );
+        service_ending_handle   = response[ 3 ] | ( response[ 4 ] << 8 );
     }
 
-    std::uint16_t starting_handle;
-    std::uint16_t ending_handle;
+    std::uint16_t service_starting_handle;
+    std::uint16_t service_ending_handle;
 
     static std::uint8_t low( std::uint16_t h )
     {
@@ -57,7 +57,7 @@ BOOST_AUTO_TEST_SUITE( service_definition )
     BOOST_FIXTURE_TEST_CASE( service_definition_over_le_as_primary_service, discover_primary_service )
     {
         BOOST_CHECK_EQUAL( response[ 0 ], 0x07 );
-        BOOST_CHECK_LT( starting_handle, ending_handle );
+        BOOST_CHECK_LT( service_starting_handle, service_ending_handle );
     }
 
     /*
@@ -78,6 +78,12 @@ struct characteristic_declaration
     std::uint16_t   uuid;
 };
 
+struct handle_uuid_pair
+{
+    std::uint16_t handle;
+    std::uint16_t uuid;
+};
+
 struct discover_all_characteristics : discover_primary_service
 {
     discover_all_characteristics()
@@ -85,8 +91,8 @@ struct discover_all_characteristics : discover_primary_service
         // read by type request
         l2cap_input( {
             0x08,
-            low( starting_handle ), high( starting_handle ),
-            low( ending_handle ), high( ending_handle ),
+            low( service_starting_handle ), high( service_starting_handle ),
+            low( service_ending_handle ), high( service_ending_handle ),
             0x03, 0x28 // <<characteristic>>
         });
 
@@ -153,7 +159,86 @@ BOOST_AUTO_TEST_SUITE( characteristic_declaration_tests )
 
 BOOST_AUTO_TEST_SUITE_END()
 
-BOOST_AUTO_TEST_SUITE( characteristic_descriptors )
+struct disconver_all_descriptors : discover_all_characteristics
+{
 
+    disconver_all_descriptors()
+    {
+        csc_measurement_client_configuration = find_client_characteristic_configuration(
+            csc_measurement.value_attribute_handle + 1, csc_feature.handle - 1);
+        sc_control_point_client_configuration = find_client_characteristic_configuration(
+            cs_control_point.value_attribute_handle + 1, service_ending_handle );
+    }
+
+    std::vector< handle_uuid_pair > find_information_request( std::uint16_t start_handle, std::uint16_t end_handle )
+    {
+        l2cap_input({
+            // Find information request
+            0x04, low( start_handle ), high( start_handle ), low( end_handle ), high( end_handle )
+        });
+
+        BOOST_REQUIRE_EQUAL( response[ 0 ], 0x05 ); // response opcode
+        BOOST_REQUIRE_EQUAL( response[ 1 ], 0x01 ); // Format
+        BOOST_REQUIRE_EQUAL( ( response_size - 2 ) % 4, 0 );
+
+        std::vector< handle_uuid_pair > result;
+        for ( const std::uint8_t* p = &response[ 2 ]; p != begin() + response_size; p += 4 )
+        {
+            result.push_back( handle_uuid_pair{ uint16( p ), uint16( p + 2 ) } );
+        }
+
+        return result;
+    }
+
+    handle_uuid_pair find_client_characteristic_configuration( std::uint16_t start_handle, std::uint16_t end_handle )
+    {
+        const auto descriptors = find_information_request( start_handle, end_handle );
+
+        const auto pos = std::find_if( descriptors.begin(), descriptors.end(),
+            []( handle_uuid_pair b ) -> bool {
+                return b.uuid == 0x2902;
+            });
+
+        BOOST_REQUIRE( pos != descriptors.end() );
+        return *pos;
+    }
+
+    std::vector< std::uint8_t > att_read( std::uint16_t handle )
+    {
+        l2cap_input({
+            0x0a, low( handle ), high( handle )
+        });
+
+        BOOST_REQUIRE_EQUAL( response[ 0 ], 0x0b ); // response opcode
+        BOOST_REQUIRE_GT( response_size, 1 );
+
+        return std::vector< std::uint8_t >( begin() + 1, begin() + response_size );
+    }
+
+    handle_uuid_pair csc_measurement_client_configuration;
+    handle_uuid_pair sc_control_point_client_configuration;
+};
+
+BOOST_AUTO_TEST_SUITE( characteristic_descriptors_tests )
+
+    BOOST_FIXTURE_TEST_CASE( csc_measurement_client_characteristic_configuration_descriptor, disconver_all_descriptors )
+    {
+        const auto value = att_read( csc_measurement_client_configuration.handle );
+
+        BOOST_CHECK_EQUAL( value.size(), 2 );
+        const std::uint16_t config_value = uint16( &value[ 0 ] );
+
+        BOOST_CHECK( config_value == 0x0000 || config_value == 0x0001 );
+    }
+
+    BOOST_FIXTURE_TEST_CASE( sc_control_point_client_characteristic_configuration_descriptor, disconver_all_descriptors )
+    {
+        const auto value = att_read( sc_control_point_client_configuration.handle );
+
+        BOOST_CHECK_EQUAL( value.size(), 2 );
+        const std::uint16_t config_value = uint16( &value[ 0 ] );
+
+        BOOST_CHECK( config_value == 0x0000 || config_value == 0x0002 );
+    }
 
 BOOST_AUTO_TEST_SUITE_END()
