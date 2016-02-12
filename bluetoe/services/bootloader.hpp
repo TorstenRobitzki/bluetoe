3f13c74a593306c58aa243233d6e636e5cb47d0a
Torsten Robitzki <Torsten@Robitzki.de>
2016-02-12 12:36:07 +0100

    set flash address

--- bluetoe/services/bootloader.hpp
+++ bluetoe/services/bootloader.hpp
@@ -8,6 +8,7 @@ namespace bluetoe
     namespace bootloader {
         namespace details {
             struct handler_meta_type {};
+            struct memory_region_meta_type {};
         };
 
         template < std::size_t PageSize >
@@ -15,6 +16,35 @@ namespace bluetoe
 
         template < std::size_t PageAlign >
         struct page_align {};
+
+        template < std::uintptr_t Start, std::uintptr_t End >
+        struct memory_region {};
+
+        template < typename ... Regions >
+        struct white_list;
+
+        template < std::uintptr_t Start, std::uintptr_t End, typename ... Regions >
+        struct white_list< memory_region< Start, End >, Regions ... >
+        {
+            static bool acceptable( std::uintptr_t addr )
+            {
+                return Start <= addr && addr < End
+                    || white_list< Regions... >::acceptable( addr );
+            }
+
+            typedef details::memory_region_meta_type meta_type;
+        };
+
+        template <>
+        struct white_list<>
+        {
+            static bool acceptable( std::uintptr_t )
+            {
+                return false;
+            }
+
+            typedef details::memory_region_meta_type meta_type;
+        };
 
         template < typename UserHandler >
         struct handler {
@@ -28,7 +58,9 @@ namespace bluetoe
 
         enum att_error_codes : std::uint8_t {
             no_operation_in_progress = bluetoe::error_codes::application_error_start,
-            invalid_opcode
+            invalid_opcode,
+            start_address_invalid,
+            start_address_not_accaptable
         };
 
         enum error_codes : std::uint8_t {};
@@ -38,18 +70,25 @@ namespace bluetoe
             enum opcode : std::uint8_t
             {
                 get_version,
-                get_address_size,
-                set_address,
+                flash_address,
+                undefined_opcode = 0xff
             };
 
             enum response_code : std::uint8_t
             {
-                version_response = 1
+                version_response = 1,
+                address_accepted
             };
 
-            template < typename UserHandler >
+            template < typename UserHandler, typename MemRegions >
             struct handler : UserHandler
             {
+                handler()
+                    : opcode( undefined_opcode )
+                    , start_address( 0 )
+                {
+                }
+
                 std::pair< std::uint8_t, bool > bootloader_write_control_point( std::size_t write_size, const std::uint8_t* value )
                 {
                     if ( write_size < 1 )
@@ -61,6 +100,29 @@ namespace bluetoe
                     {
                     case get_version:
                         return std::pair< std::uint8_t, bool >{ bluetoe::error_codes::success, true };
+                    case flash_address:
+                        {
+                            if ( write_size != 1 + sizeof( std::uint8_t* ) )
+                            {
+                                opcode = undefined_opcode;
+                                return std::pair< std::uint8_t, bool >{ start_address_invalid, false };
+                            }
+
+                            start_address = 0;
+                            for ( const std::uint8_t* rbegin = value + write_size, *rend = value +1; rbegin != rend; --rbegin )
+                            {
+                                start_address = start_address << 8;
+                                start_address |= *( rbegin -1 );
+                            }
+
+                            if ( !MemRegions::acceptable( start_address ) )
+                            {
+                                opcode = undefined_opcode;
+                                return std::pair< std::uint8_t, bool >{ start_address_not_accaptable, false };
+                            }
+
+                            return std::pair< std::uint8_t, bool >{ bluetoe::error_codes::success, false };
+                        }
                     }
 
                     return std::pair< std::uint8_t, bool >{ att_error_codes::invalid_opcode, false };
@@ -95,17 +157,23 @@ namespace bluetoe
                     return 0;
                 }
 
-                std::uint8_t opcode;
+                std::uint8_t    opcode;
+                std::uintptr_t  start_address;
             };
 
             template < typename ... Options >
             struct calculate_service {
+                using mem_regions  = typename bluetoe::details::find_by_meta_type< memory_region_meta_type, Options... >::type;
+
+                static_assert( !std::is_same< bluetoe::details::no_such_type, mem_regions >::value,
+                    "Please use white_list or black_list, to define accessable memory regions." );
+
                 using user_handler = typename bluetoe::details::find_by_meta_type< handler_meta_type, Options... >::type;
 
                 static_assert( !std::is_same< bluetoe::details::no_such_type, user_handler >::value,
                     "To use the bootloader, please provide a handler<> that fullfiles the requirements documented with bootloader_handler_prototype." );
 
-                using implementation = handler< typename user_handler::user_handler >;
+                using implementation = handler< typename user_handler::user_handler, mem_regions >;
 
                 using type = bluetoe::service<
                     bluetoe::bootloader::service_uuid,
