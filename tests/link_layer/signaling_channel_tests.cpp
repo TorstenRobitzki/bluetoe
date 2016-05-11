a125cb7ab52708aaf576daf528564a9075dac1c8
Torsten Robitzki <Torsten@Robitzki.de>
2016-05-11 09:33:39 +0200

    fixing assert

--- tests/link_layer/signaling_channel_tests.cpp
+++ tests/link_layer/signaling_channel_tests.cpp
@@ -11,6 +11,7 @@ struct channel : bluetoe::l2cap::signaling_channel<>
 
     void signaling_channel_input( std::initializer_list< std::uint8_t > pdu, std::initializer_list< std::uint8_t > expected )
     {
+        out_size = sizeof( buffer );
         signaling_channel::signaling_channel_input( pdu.begin(), pdu.size(), buffer, out_size );
 
         BOOST_REQUIRE_EQUAL_COLLECTIONS( expected.begin(), expected.end(), &buffer[ 0 ], &buffer[ out_size ] );
@@ -18,6 +19,7 @@ struct channel : bluetoe::l2cap::signaling_channel<>
 
     void signaling_channel_output( std::initializer_list< std::uint8_t > expected )
     {
+        out_size = sizeof( buffer );
         signaling_channel::signaling_channel_output( buffer, out_size );
         BOOST_REQUIRE_EQUAL_COLLECTIONS( expected.begin(), expected.end(), &buffer[ 0 ], &buffer[ out_size ] );
     }
