46e6c2b07c51987d9f134de09d568169dfe14e58
Torsten Robitzki <Torsten@Robitzki.de>
2016-06-02 13:29:43 +0200

    use different address type in address report test

--- tests/link_layer/connection_callbacks_tests.cpp
+++ tests/link_layer/connection_callbacks_tests.cpp
@@ -185,13 +185,27 @@ BOOST_FIXTURE_TEST_CASE( connection_details_reported_when_connection_is_establis
 
 BOOST_FIXTURE_TEST_CASE( addresses_reported_when_connection_established, link_layer_only_connect_callback )
 {
-    respond_to( 37, valid_connection_request_pdu );
+    respond_to( 37, {
+        0x85, 0x22,                         // header
+        0x3c, 0x1c, 0x62, 0x92, 0xf0, 0x48, // InitA: 48:f0:92:62:1c:3c (public)
+        0x47, 0x11, 0x08, 0x15, 0x0f, 0xc0, // AdvA:  c0:0f:15:08:11:47 (random)
+        0x5a, 0xb3, 0x9a, 0xaf,             // Access Address
+        0x08, 0x81, 0xf6,                   // CRC Init
+        0x03,                               // transmit window size
+        0x0b, 0x00,                         // window offset
+        0x18, 0x00,                         // interval (30ms)
+        0x00, 0x00,                         // slave latency
+        0x48, 0x00,                         // connection timeout (720ms)
+        0xff, 0xff, 0xff, 0xff, 0x1f,       // used channel map
+        0xaa                                // hop increment and sleep clock accuracy (10 and 50ppm)
+    } );
+
     add_connection_event_respond( { 0, 1 } );
     run();
 
     const auto reported_addresses = only_connect_callback.reported_addresses;
-    BOOST_CHECK_EQUAL( reported_addresses.remote_address(), bluetoe::link_layer::address( { 0x3c, 0x1c, 0x62, 0x92, 0xf0, 0x48 } ) );
-    BOOST_CHECK_EQUAL( reported_addresses.local_address(),  bluetoe::link_layer::address( { 0x47, 0x11, 0x08, 0x15, 0x0f, 0xc0 } ) );
+    BOOST_CHECK_EQUAL( reported_addresses.remote_address(), bluetoe::link_layer::public_device_address( { 0x3c, 0x1c, 0x62, 0x92, 0xf0, 0x48 } ) );
+    BOOST_CHECK_EQUAL( reported_addresses.local_address(),  bluetoe::link_layer::random_device_address( { 0x47, 0x11, 0x08, 0x15, 0x0f, 0xc0 } ) );
 }
 
 BOOST_FIXTURE_TEST_CASE( connection_update_not_called_by_default, link_layer_only_changed_callback )
