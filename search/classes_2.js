4f880fed3a561d828edf47e13a19b90e36eb1462
Documentation Builder <travis@robitzki.de>
2018-05-16 11:43:34 +0200

    Automated documentation build for changeset 4e3dda34f044603a1cfd1b3b45952f7eaee8d760.

--- search/classes_2.js
+++ search/classes_2.js
@@ -13,6 +13,8 @@ var searchData=
   ['connection_5fdetails',['connection_details',['../classbluetoe_1_1link__layer_1_1connection__details.html',1,'bluetoe::link_layer']]],
   ['connection_5fevent_5fcallback',['connection_event_callback',['../structbluetoe_1_1link__layer_1_1connection__event__callback.html',1,'bluetoe::link_layer']]],
   ['crank_5frevolution_5fdata_5fsupported',['crank_revolution_data_supported',['../structbluetoe_1_1csc_1_1crank__revolution__data__supported.html',1,'bluetoe::csc']]],
+  ['cstring_5fvalue',['cstring_value',['../structbluetoe_1_1cstring__value.html',1,'bluetoe']]],
   ['cstring_5fwrapper',['cstring_wrapper',['../structbluetoe_1_1cstring__wrapper.html',1,'bluetoe']]],
+  ['cstring_5fwrapper_3c_20cstring_5fvalue_3c_20name_20_3e_20_3e',['cstring_wrapper&lt; cstring_value&lt; Name &gt; &gt;',['../structbluetoe_1_1cstring__wrapper.html',1,'bluetoe']]],
   ['cycling_5fspeed_5fand_5fcadence_5fhandler_5fprototype',['cycling_speed_and_cadence_handler_prototype',['../classbluetoe_1_1cycling__speed__and__cadence__handler__prototype.html',1,'bluetoe']]]
 ];
