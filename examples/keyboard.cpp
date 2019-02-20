9bd43fc8847d68a58d3409601c61be9377c2ffd2
Torsten Robitzki <Torsten@Robitzki.de>
2019-02-20 09:39:43 +0100

    use fixed_blob_value to implement report map characteristic

--- examples/keyboard.cpp
+++ examples/keyboard.cpp
@@ -4,6 +4,54 @@
 #include <bluetoe/services/dis.hpp>
 #include <bluetoe/services/hid.hpp>
 
+ // shamless stolen from the Nordic examples
+static const std::uint8_t report_map_data[] =
+{
+    0x05, 0x01,       // Usage Page (Generic Desktop)
+    0x09, 0x06,       // Usage (Keyboard)
+    0xA1, 0x01,       // Collection (Application)
+    0x05, 0x07,       // Usage Page (Key Codes)
+    0x19, 0xe0,       // Usage Minimum (224)
+    0x29, 0xe7,       // Usage Maximum (231)
+    0x15, 0x00,       // Logical Minimum (0)
+    0x25, 0x01,       // Logical Maximum (1)
+    0x75, 0x01,       // Report Size (1)
+    0x95, 0x08,       // Report Count (8)
+    0x81, 0x02,       // Input (Data, Variable, Absolute)
+
+    0x95, 0x01,       // Report Count (1)
+    0x75, 0x08,       // Report Size (8)
+    0x81, 0x01,       // Input (Constant) reserved byte(1)
+
+    0x95, 0x05,       // Report Count (5)
+    0x75, 0x01,       // Report Size (1)
+    0x05, 0x08,       // Usage Page (Page# for LEDs)
+    0x19, 0x01,       // Usage Minimum (1)
+    0x29, 0x05,       // Usage Maximum (5)
+    0x91, 0x02,       // Output (Data, Variable, Absolute), Led report
+    0x95, 0x01,       // Report Count (1)
+    0x75, 0x03,       // Report Size (3)
+    0x91, 0x01,       // Output (Data, Variable, Absolute), Led report padding
+
+    0x95, 0x06,       // Report Count (6)
+    0x75, 0x08,       // Report Size (8)
+    0x15, 0x00,       // Logical Minimum (0)
+    0x25, 0x65,       // Logical Maximum (101)
+    0x05, 0x07,       // Usage Page (Key codes)
+    0x19, 0x00,       // Usage Minimum (0)
+    0x29, 0x65,       // Usage Maximum (101)
+    0x81, 0x00,       // Input (Data, Array) Key array(6 bytes)
+
+    0x09, 0x05,       // Usage (Vendor Defined)
+    0x15, 0x00,       // Logical Minimum (0)
+    0x26, 0xFF, 0x00, // Logical Maximum (255)
+    0x75, 0x08,       // Report Count (2)
+    0x95, 0x02,       // Report Size (8 bit)
+    0xB1, 0x02,       // Feature (Data, Variable, Absolute)
+
+    0xC0              // End Collection (Application)
+};
+
 class keyboard_handler
 {
 public:
@@ -36,15 +84,6 @@ public:
 
     std::uint8_t report_read_handler( std::size_t read_size, std::uint8_t* out_buffer, std::size_t& out_size )
     {
-        (void)read_size;
-        (void)out_buffer;
-        (void)out_size;
-        return bluetoe::error_codes::success;
-    }
-
-    std::uint8_t report_map_read_handler( std::size_t offset, std::size_t read_size, std::uint8_t* out_buffer, std::size_t& out_size )
-    {
-        (void)offset;
         (void)read_size;
         (void)out_buffer;
         (void)out_size;
@@ -126,7 +165,7 @@ using hid_service = bluetoe::service<
     >,
     bluetoe::characteristic<
         bluetoe::hid::report_map_uuid,
-        bluetoe::mixin_read_blob_handler< keyboard_handler, &keyboard_handler::report_map_read_handler >
+        bluetoe::fixed_blob_value< report_map_data, sizeof( report_map_data ) >
     >,
     bluetoe::characteristic<
         bluetoe::hid::boot_keyboard_input_report_uuid,
