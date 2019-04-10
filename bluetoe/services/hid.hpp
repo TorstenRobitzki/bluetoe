bde81cdf100274448979b9872d93245a0671cee9
Torsten Robitzki <Torsten@Robitzki.de>
2019-04-10 13:38:48 +0200

    report descriptors

--- bluetoe/services/hid.hpp
+++ bluetoe/services/hid.hpp
@@ -1,6 +1,9 @@
 #ifndef BLUETOE_SERVICES_HID_HPP
 #define BLUETOE_SERVICES_HID_HPP
 
+#include <bluetoe/service.hpp>
+#include <bluetoe/characteristic.hpp>
+
 namespace bluetoe {
 
     namespace hid {
@@ -17,6 +20,45 @@ namespace bluetoe {
         using boot_mouse_input_report_uuid      = characteristic_uuid16< 0x2A33 >;
         using hid_information_uuid              = characteristic_uuid16< 0x2A4A >;
         using hid_control_point_uuid            = characteristic_uuid16< 0x2A4C >;
+
+        static constexpr std::uint16_t report_reference_descriptor_uuid = 0x2908;
+
+        enum class report_type : std::uint8_t {
+            input = 1,
+            output = 2,
+            feature = 3
+        };
+
+        namespace details {
+            template < std::uint8_t ReportID, report_type Type >
+            struct report_reference_impl {
+                static const std::uint8_t data[ 2 ];
+
+                using descriptor = bluetoe::descriptor< report_reference_descriptor_uuid, data, sizeof( data ) >;
+            };
+
+            template < std::uint8_t ReportID, report_type Type >
+            const std::uint8_t report_reference_impl< ReportID, Type >::data[ 2 ] = { ReportID, static_cast< std::uint8_t >( Type ) };
+
+        }
+
+        /**
+         * @brief an Input Report Reference Descriptor
+         */
+        template < std::uint8_t ReportID >
+        using input_report_reference = typename details::report_reference_impl< ReportID, report_type::input >::descriptor;
+
+        /**
+         * @brief an Output Report Reference Descriptor
+         */
+        template < std::uint8_t ReportID >
+        using output_report_reference = typename details::report_reference_impl< ReportID, report_type::output >::descriptor;
+
+        /**
+         * @brief a Feature Report Reference Descriptor
+         */
+        template < std::uint8_t ReportID >
+        using feature_report_reference = typename details::report_reference_impl< ReportID, report_type::feature >::descriptor;
     }
 }
 
