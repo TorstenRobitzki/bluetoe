65838525804dadfe24399fa08becfc3d9a09ce03
Torsten Robitzki <Torsten@Robitzki.de>
2022-02-08 08:49:50 +0100

    fix warning about unused parameter

--- examples/assert/assert.cpp
+++ examples/assert/assert.cpp
@@ -2,7 +2,7 @@
 
 extern "C" void HardFault_Handler() __attribute__ ((noreturn));
 
-extern "C" void __assert_hash_func( assert_hash_type hash )
+extern "C" void __assert_hash_func( assert_hash_type )
 {
     HardFault_Handler();
 }
