5cabbe6aeaba1179add7bacd75f29c26d5b788ff
Torsten Robitzki <Torsten@Robitzki.de>
2022-04-27 14:13:49 +0200

    add tests for Channel Map Update procedure

--- tests/link_layer/ll_control_tests.cpp
+++ tests/link_layer/ll_control_tests.cpp
@@ -104,3 +104,60 @@ BOOST_FIXTURE_TEST_CASE( do_not_respond_to_UNKNOWN_RSP_even_if_broken, unconnect
         "do_not_respond_to_UNKNOWN_RSP"
     );
 }
+
+/*
+ * Example from core spec Vol6, Part B, 5.1.2
+ */
+BOOST_FIXTURE_TEST_CASE( Channel_Map_Update_procedure, unconnected )
+{
+    respond_to( 37, valid_connection_request_pdu );
+
+    ll_control_pdu(
+        {
+            0x01,                           // LL_CHANNEL_MAP_IND
+            0xff, 0xf7, 0xff, 0xff, 0x1f,   // new channel map: all channels enabled except channel 11
+            0x64, 0x00                      // Instance: 100dez
+        }
+    );
+
+    ll_empty_pdus( 101 );
+
+    run();
+
+    BOOST_CHECK_EQUAL( connection_events()[ 99 ].channel, 1u );
+    BOOST_CHECK_EQUAL( connection_events()[ 100 ].channel, 12u );
+    BOOST_CHECK_EQUAL( connection_events()[ 101 ].channel, 21u );
+}
+
+BOOST_FIXTURE_TEST_CASE( Channel_Map_Update_procedure_With_Latency, unconnected )
+{
+    respond_to( 37, {
+        0xc5, 0x22,                         // header
+        0x3c, 0x1c, 0x62, 0x92, 0xf0, 0x48, // InitA: 48:f0:92:62:1c:3c (random)
+        0x47, 0x11, 0x08, 0x15, 0x0f, 0xc0, // AdvA:  c0:0f:15:08:11:47 (random)
+        0x5a, 0xb3, 0x9a, 0xaf,             // Access Address
+        0x08, 0x81, 0xf6,                   // CRC Init
+        0x03,                               // transmit window size
+        0x0b, 0x00,                         // window offset
+        0x18, 0x00,                         // interval (30ms)
+        0x28, 0x00,                         // peripheral latency 40
+        0x48, 0x01,                         // connection timeout (3280ms)
+        0xff, 0xff, 0xff, 0xff, 0x1f,       // used channel map
+        0xaa                                // hop increment and sleep clock accuracy (10 and 50ppm)
+    } );
+
+    ll_control_pdu(
+        {
+            0x01,                           // LL_CHANNEL_MAP_IND
+            0xff, 0xf7, 0xff, 0xff, 0x1f,   // new channel map: all channels enabled except channel 11
+            0x64, 0x00                      // Instance: 100dez
+        }
+    );
+
+    ll_empty_pdus( 5 );
+
+    run();
+
+    // First event at instant 0; second at 41, third at 82; 4th at 100
+    BOOST_CHECK_EQUAL( connection_events()[ 3 ].channel, 12u );
+}
