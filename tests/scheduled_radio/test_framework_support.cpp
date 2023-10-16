3430b13ddd7277e7827aaac9aa5dcb2e56a0449b
Torsten Robitzki <Torsten@Robitzki.de>
2023-10-16 11:31:34 +0200

    assert when unimplemented stub is called

--- tests/scheduled_radio/test_framework_support.cpp
+++ tests/scheduled_radio/test_framework_support.cpp
@@ -1,29 +1,33 @@
 #include "test_framework_support.hpp"
 
 #include "system_nrf52.h"
+#include <cassert>
 
-extern "C" void _close(void) {}
-extern "C" void _fstat(void) {}
-extern "C" void _getpid(void) {}
-extern "C" void _gettimeofday(void) {}
-extern "C" void _isatty(void) {}
-extern "C" void _kill(void) {}
-extern "C" void _lseek(void) {}
-extern "C" void _open(void) {}
-extern "C" void _read(void) {}
-extern "C" void _write(void) {}
+extern "C" void _close(void) { assert( false ); }
+extern "C" void _fstat(void) { assert( false ); }
+extern "C" void _getpid(void) { assert( false ); }
+extern "C" void _gettimeofday(void) { assert( false ); }
+extern "C" void _isatty(void) { assert( false ); }
+extern "C" void _kill(void) { assert( false ); }
+extern "C" void _lseek(void) { assert( false ); }
+extern "C" void _open(void) { assert( false ); }
+extern "C" void _read(void) { assert( false ); }
+extern "C" void _write(void) { assert( false ); }
 
 std::ostream& Catch::cout()
 {
+     assert( false );
     return *static_cast< std::ostream* >( 0 );
 }
 
 std::ostream& Catch::cerr()
 {
+     assert( false );
     return *static_cast< std::ostream* >( 0 );
 }
 
 std::ostream& Catch::clog()
 {
+     assert( false );
     return *static_cast< std::ostream* >( 0 );
 }
