d3b70af30b72c7f91f7f216251bb3b3e09a424dc
Torsten Robitzki <Torsten@Robitzki.de>
2023-10-24 16:55:19 +0200

    provide output for the streams

--- tests/scheduled_radio/test_framework_support.cpp
+++ tests/scheduled_radio/test_framework_support.cpp
@@ -1,7 +1,11 @@
 #include "test_framework_support.hpp"
 
 #include "system_nrf52.h"
+#include <nrf.h>
+
 #include <cassert>
+#include <streambuf>
+#include <ostream>
 
 extern "C" void _close(void) { assert( false ); }
 extern "C" void _fstat(void) { assert( false ); }
@@ -14,20 +18,66 @@ extern "C" void _open(void) { assert( false ); }
 extern "C" void _read(void) { assert( false ); }
 extern "C" void _write(void) { assert( false ); }
 
+
+template < std::uint32_t TxPin >
+class uart_stream_buf : public std::basic_streambuf< char >
+{
+public:
+    uart_stream_buf()
+        : uarte_( *NRF_UARTE1 )
+    {
+        NRF_P0->OUTSET = 1 << TxPin;
+
+        NRF_P0->PIN_CNF[ TxPin ] =
+            ( GPIO_PIN_CNF_DIR_Output << GPIO_PIN_CNF_DIR_Pos )
+          | ( GPIO_PIN_CNF_INPUT_Connect << GPIO_PIN_CNF_INPUT_Pos )
+          | ( GPIO_PIN_CNF_PULL_Disabled << GPIO_PIN_CNF_PULL_Pos )
+          | ( GPIO_PIN_CNF_DRIVE_S0S1 << GPIO_PIN_CNF_DRIVE_Pos );
+
+        uarte_.ENABLE    = ( UARTE_ENABLE_ENABLE_Enabled << UARTE_ENABLE_ENABLE_Pos );
+
+        uarte_.BAUDRATE  = UART_BAUDRATE_BAUDRATE_Baud115200;
+        uarte_.PSEL.TXD  = TxPin;
+
+        setp( std::begin( buffer_ ), std::end( buffer_ ) );
+    }
+
+private:
+    virtual int_type overflow(int_type ch) override
+    {
+        uarte_.EVENTS_ENDTX = 0;
+
+        uarte_.TXD.MAXCNT = pptr() - pbase();
+        uarte_.TXD.PTR    = reinterpret_cast< std::uint32_t >( &buffer_[ 0 ] );
+
+        uarte_.TASKS_STARTTX = 1;
+
+        while ( uarte_.EVENTS_ENDTX == 0 )
+            ;
+
+        setp( std::begin( buffer_ ), std::end( buffer_ ) );
+
+        return std::char_traits< char >::to_char_type( sputc( ch ) );
+    }
+
+    NRF_UARTE_Type& uarte_ = *NRF_UARTE1;
+    char buffer_[ 16 ];
+};
+
 std::ostream& Catch::cout()
 {
-     assert( false );
-    return *static_cast< std::ostream* >( 0 );
+    static uart_stream_buf< 6 > buf;
+    static std::ostream out( &buf );
+
+    return out;
 }
 
 std::ostream& Catch::cerr()
 {
-     assert( false );
-    return *static_cast< std::ostream* >( 0 );
+    return Catch::cout();
 }
 
 std::ostream& Catch::clog()
 {
-     assert( false );
-    return *static_cast< std::ostream* >( 0 );
+    return Catch::cout();
 }
